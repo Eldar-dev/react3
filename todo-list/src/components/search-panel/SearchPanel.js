ded036721df66436917fb27c792f0e0974f75c3f
Eldar <ilona0179@mail.ru>
2020-04-04 03:29:54 +0300

    new

---
+++ todo-list/src/components/search-panel/SearchPanel.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./search-panel.css";
+
+const SearchPanel = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="form-control search-input"
+        id="search"
+        placeholder="type to search"
+      />
+    </div>
+  );
+};
+
+export { SearchPanel };
